5ae64f8e927c4082448bb8a81e299970038dcab5
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-21 00:18:09 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,9 +12,11 @@ class Song < ActiveRecord::Base
   end
 
   def genre_name=(genre)
+    self.genre = Genre.find_or_create_by(name: name)
   end
 
   def genre_name
+    self.genre ? self.genre.name :nil
   end
 
   def note_contents=(notes)

ea99a9b33af0c18d4ffdec5c9e0e24bea9f87624
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-21 00:16:51 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,10 +5,9 @@ class Song < ActiveRecord::Base
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
-  end 
+  end
 
-  def artist_name 
-
+  def artist_name
+    self.artist ? self.artist.name : nil
   end
 end
-
